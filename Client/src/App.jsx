a3cd819340f28bdcb1722decde4c2c43031e54aa
rayleigh420 <nhatduy0409@gmail.com>
2023-04-21 20:13:57 +0700

    Create naviagtion components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,16 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navigation from './components/navigation'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <button className='text-blue-600'>Hello</button>
-        <button className="btn">Hello with Daisy UI</button>
-      </div>
+      <Navigation />
     </>
   )
 }
