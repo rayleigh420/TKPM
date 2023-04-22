25ea7bce75e0f7e711ebccb9b48bfd1f2c0277eb
rayleigh420 <nhatduy0409@gmail.com>
2023-04-22 20:44:47 +0700

    Book card components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,6 +10,13 @@ function App() {
   return (
     <>
       <Navigation />
+      <div className="artboard phone-6 px-[140px]">
+        <div className='w-[158px] h-[270px] rounded-[10px] cursor-pointer'>
+          <img src="https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2" className='w-full h-[230px] overflow-hidden'></img>
+          <p className='text-[#e0e0e0] text-[15.2px] font-semibold leaiding-[18.24px] tracking-[-0.304px] mt-1'>Boi hoi gia</p>
+        </div>
+      </div>
+      <main className='px-[140px]'>Hello</main>
       <Footer />
     </>
   )
