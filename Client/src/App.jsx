2e7a49efcc4e9d8724057914474d9d4da24c1b8d
rayleigh420 <nhatduy0409@gmail.com>
2023-04-23 09:49:34 +0700

    Test layout for list book

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,18 +5,61 @@ import './App.css'
 import Navigation from './components/navigation'
 import Footer from './components/Footer'
 
+const data = [
+
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  },
+  {
+    src: 'https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2',
+    name: 'Boi hoi gia'
+  }
+]
+
 function App() {
 
   return (
     <>
       <Navigation />
-      <div className="artboard phone-6 px-[140px]">
-        <div className='w-[158px] h-[270px] rounded-[10px] cursor-pointer'>
-          <img src="https://i.truyenvua.com/ebook/190x247/boi-hoi-gia_1663135088.jpg?gt=hdfgdfg&mobile=2" className='w-full h-[230px] overflow-hidden'></img>
-          <p className='text-[#e0e0e0] text-[15.2px] font-semibold leaiding-[18.24px] tracking-[-0.304px] mt-1'>Boi hoi gia</p>
-        </div>
+      <div className="px-[135px] flex gap-5 flex-wrap mt-[30px]">
+        {
+          data.map(item => (
+            <div className='w-[158px] h-[270px] rounded-[7px] cursor-pointer'>
+              <img src={item.src} className='w-full h-[230px]' />
+              <p className='text-[#e0e0e0] text-[15.2px] font-semibold leaiding-[18.24px] tracking-[-0.304px] mt-1'>{item.name}</p>
+            </div>
+          ))
+        }
       </div>
-      <main className='px-[140px]'>Hello</main>
       <Footer />
     </>
   )
