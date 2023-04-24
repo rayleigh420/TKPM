e30cdf26c147b1e61e4f9f6cbf115ceec25426b5
rayleigh420 <nhatduy0409@gmail.com>
2023-04-24 20:33:13 +0700

    Tab convert

--- Client/src/pages/Book.jsx
+++ Client/src/pages/Book.jsx
@@ -3,6 +3,13 @@ import { useState } from "react"
 const Book = () => {
     const [tab, setTab] = useState(false)
 
+    const handleTabAbout = () => {
+        setTab(false)
+    }
+
+    const handleTabLocation = () => {
+        setTab(true)
+    }
 
     return (
         <>
@@ -24,43 +31,68 @@ const Book = () => {
                 </div>
                 <div className="tabs w-full tab-bordered flex flex-col items-center">
                     <div className="w-[62%]">
-                        <a className="pb-[40px] text-[#ffffff] text-[20px] font-semibold leading-[24px] tracking-[-0.4px] tab tab-bordered tab-active">About</a>
-                        <a className="pb-[40px] text-[#ffffff] text-[20px] font-semibold leading-[24px] tracking-[-0.4px] tab tab-bordered">Location</a>
+                        <a className={"pb-[40px] text-[#ffffff] text-[20px] font-semibold leading-[24px] tracking-[-0.4px] tab tab-bordered" + (tab ? "tab-active" : "")} onClick={handleTabAbout}>About</a>
+                        <a className={"pb-[40px] text-[#ffffff] text-[20px] font-semibold leading-[24px] tracking-[-0.4px] tab tab-bordered" + (!tab ? "tab-active" : "")} onClick={handleTabLocation}>Location</a>
                     </div>
                 </div>
-                <div className="w-[62%] mt-[30px]">
-                    <h4 className="text-[#ffffff] text-[20px] font-bold leading-[24px] tracking-[-0.4px] mb-[20px]">Information</h4>
-                    <div className="overflow-x-auto">
-                        <table className="table w-full border-nones">
-                            <thead>
-                                <tr>
-                                    <th>Pages</th>
-                                    <th>Licensed from</th>
-                                    <th>Number of renter</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>83 Chapters</td>
-                                    <td>Si Reubereu / TITAN</td>
-                                    <td>100</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="divider bordered border-[#ffffff]"></div>
-                    <div className="text-[#e0e0e0] leading-[24px] tracking-[-0.32px]">
-                        <h4 className="text-[#ffffff] text-[20px] font-bold leading-[24px] tracking-[-0.4px] mb-[25px]">Details</h4>
-                        Status in Korean: Completed @ 254 chapters <br />
-                        <br />
-                        Translator: Yeniverse <br />
-                        <br />
-                        Editor: Dot, SoSam <br />
-                        <br />
-                        Release rate: 5 chapters a week with an occasional bonus 6th chapter
-                    </div>
-                    <div className="divider bordered border-[#ffffff]"></div>
-                </div>
+                {
+                    !tab ? (
+                        <div className="w-[62%] mt-[30px]">
+                            <h4 className="text-[#ffffff] text-[20px] font-bold leading-[24px] tracking-[-0.4px] mb-[20px]">Information</h4>
+                            <div className="overflow-x-auto">
+                                <table className="table w-full border-nones">
+                                    <thead>
+                                        <tr>
+                                            <th>Pages</th>
+                                            <th>Licensed from</th>
+                                            <th>Number of renter</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>83 Chapters</td>
+                                            <td>Si Reubereu / TITAN</td>
+                                            <td>100</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="divider bordered border-[#ffffff]"></div>
+                            <div className="text-[#e0e0e0] leading-[24px] tracking-[-0.32px]">
+                                <h4 className="text-[#ffffff] text-[20px] font-bold leading-[24px] tracking-[-0.4px] mb-[25px]">Details</h4>
+                                Status in Korean: Completed @ 254 chapters <br />
+                                <br />
+                                Translator: Yeniverse <br />
+                                <br />
+                                Editor: Dot, SoSam <br />
+                                <br />
+                                Release rate: 5 chapters a week with an occasional bonus 6th chapter
+                            </div>
+                            <div className="divider bordered border-[#ffffff]"></div>
+                        </div>) :
+                        (
+                            <div className="w-[62%] mt-[30px] mb-[150px]">
+                                <div className="overflow-x-auto">
+                                    <table className="table w-full border-nones">
+                                        <thead>
+                                            <tr>
+                                                <th>Pages</th>
+                                                <th>Licensed from</th>
+                                                <th>Number of renter</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>83 Chapters</td>
+                                                <td>Si Reubereu / TITAN</td>
+                                                <td>100</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        )
+                }
 
             </div >
         </>
