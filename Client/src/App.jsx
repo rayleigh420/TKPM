53f860ad314d654b5213bfec4a25811a4559f4dd
rayleigh420 <nhatduy0409@gmail.com>
2023-04-25 09:57:15 +0700

    Add profile form edit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
 import Home from './pages/home'
 import Book from './pages/Book'
+import Profile from './pages/Profile'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />} >
           <Route path='home' element={<Home />} />
           <Route path='book' element={<Book />} />
+          <Route path='profile' element={<Profile />} />
         </Route>
       </Routes>
     </>
---
+++ Client/src/pages/Profile.jsx
@@ -0,0 +1,68 @@
+const Profile = () => {
+    return (
+        <>
+            <div className="flex flex-row justify-start">
+                <div className="w-[310px] pl-[135px] mt-[100px]">
+                    <ul className="menu menu-compact flex flex-col p-0 py-[10px] px-[5px] w-[300px] border border-[#191D24] text-[#ffffff] font-medium leading-[24px] bg-[#2a303c] rounded-[10px] shadow-md ">
+                        <li><a className="text-[20px] font-semibold py-[23px] text-[#ffffff]">
+                            Le Nhat Duy
+                        </a></li>
+                        <li></li>
+                        <li>
+                            <a className="active">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                                </svg>
+                                Profile
+                            </a>
+                        </li>
+                        <li></li>
+                        <li>
+                            <a>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                                </svg>
+
+                                List rented books
+                            </a>
+                        </li>
+                        <li></li>
+                        <li>
+                            <a>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path fillRule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z" clipRule="evenodd" />
+                                </svg>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="ml-[200px]">
+                    <h1 className="mt-[50px] text-[32px] text-[#ffffff] leading-[32px] font-semibold">Your profile</h1>
+                    <div className="divider bordered border-[#ffffff]"></div>
+                    <div className="mt-[50px] w-[300px]">
+                        <div className="w-full mt-[22px] flex justify-center">
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text text-[#ffffff] font-semibold">Name</span>
+                                </label>
+                                <input value="Le Nhat Duy" type="text" placeholder="Le Nhat Duy" className="input input-bordered w-full max-w-xs" />
+                            </div>
+                        </div>
+
+                        <div className="w-full mt-[24px] flex justify-center">
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text text-[#ffffff] font-semibold">Email</span>
+                                </label>
+                                <input value="strip@gmail.com" type="email" placeholder="strip@gmail.com" className="input input-bordered w-full max-w-xs" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Profile
