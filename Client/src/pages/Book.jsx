5319ac3065c567f5b62255ad934fe97f21c089b4
rayleigh420 <nhatduy0409@gmail.com>
2023-04-25 20:49:24 +0700

    Add button add verison of book

--- Client/src/pages/Book.jsx
+++ Client/src/pages/Book.jsx
@@ -84,11 +84,19 @@ const Book = () => {
                                 <div className="overflow-x-auto">
                                     <div className="flex flex-row justify-between mb-[20px]">
                                         <h4 className="text-[#ffffff] text-[20px] font-bold leading-[24px] tracking-[-0.4px]">Status of all versions</h4>
-                                        <button className="btn btn-ghost text-[#ffffff]">
-                                            <label htmlFor="history_book" className="cursor-pointer">
-                                                Watch History Rent
-                                            </label>
-                                        </button>
+                                        <div>
+                                            <button className="btn bg-[#eeeeee] text-[#00000] mr-[10px] hover:text-[#ffffff]">
+                                                <label htmlFor="history_book" className="cursor-pointer">
+                                                    Watch History Rent
+                                                </label>
+                                            </button>
+                                            <button className="btn w-[140px] bg-gradient-to-r from-indigo-700 to-blue-700 text-[#ffffff] leading-[24px] hover:from-indigo-600 hover:to-blue-600">
+                                                <label htmlFor="" className="cursor-pointer">
+                                                    Add version
+                                                </label>
+                                            </button>
+                                        </div>
+
                                     </div>
                                     <table className="table w-full border-nones">
                                         <thead className="text-[#ffffff] font-bold text-[15px]">
