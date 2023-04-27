d3c2b08e56b32d0038beb3bc4b73e359c56d32e3
rayleigh420 <nhatduy0409@gmail.com>
2023-04-27 20:45:06 +0700

    Set ava for navbar

--- Client/src/components/Navigation.jsx
+++ Client/src/components/Navigation.jsx
@@ -48,7 +48,7 @@ const Navigation = ({ setLog }) => {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar" htmlFor="" onClick={handleOpen}>
                             <div className="w-10 rounded-full">
-                                <img src="https://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png" />
+                                <img src="https://i1.sndcdn.com/avatars-0QCRofC3yRV0mkpa-6XQLMA-t500x500.jpg" />
                             </div>
                         </label>
                         <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 font-semibold">
