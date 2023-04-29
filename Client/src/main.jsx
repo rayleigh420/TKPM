77b8f1ed1100816cd8d79fa435338153449259fb
rayleigh420 <nhatduy0409@gmail.com>
2023-04-29 10:13:51 +0700

    Create authContext

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { AuthProvider } from './context/AuthProvider.jsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,11 +19,12 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode >,
 )
