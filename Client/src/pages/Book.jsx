5cb19f17158f8e8ace2b728fecfb0c6fc6898633
rayleigh420 <nhatduy0409@gmail.com>
2023-05-05 16:55:06 +0700

    Authorization for LocataionVersion of book components

--- Client/src/pages/Book.jsx
+++ Client/src/pages/Book.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import HistoryBook from "../components/admin/HistoryBook"
 import AddVersionBook from "../components/modals/AddVersionBook"
 import RelatedBook from "../components/list/RelatedBook"
 import { Link, useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { getDetailBook } from "../api/bookApi"
+import AuthContext from "../context/AuthProvider"
+import LocationVersion from "../components/admin/LocationVersion"
 
 //status:
 
@@ -15,6 +17,8 @@ import { getDetailBook } from "../api/bookApi"
 const Book = () => {
     const [tab, setTab] = useState(false)
     const { id } = useParams()
+
+    const { auth } = useContext(AuthContext)
 
     const { data: book, isLoading, isError } = useQuery({
         queryKey: ["book", id],
@@ -58,7 +62,10 @@ const Book = () => {
                 <div className="tabs w-full tab-bordered flex flex-col items-center">
                     <div className="w-[62%]">
                         <a className={"pb-[40px] text-[#ffffff] text-[20px] font-semibold leading-[24px] tracking-[-0.4px] tab tab-bordered" + (tab ? "tab-active" : "")} onClick={handleTabAbout}>About</a>
-                        <a className={"pb-[40px] text-[#ffffff] text-[20px] font-semibold leading-[24px] tracking-[-0.4px] tab tab-bordered" + (!tab ? "tab-active" : "")} onClick={handleTabLocation}>Location</a>
+                        {
+                            auth?.role == 'admin' &&
+                            <a className={"pb-[40px] text-[#ffffff] text-[20px] font-semibold leading-[24px] tracking-[-0.4px] tab tab-bordered" + (!tab ? "tab-active" : "")} onClick={handleTabLocation}>Location</a>
+                        }
                     </div>
                 </div>
                 {
@@ -107,60 +114,61 @@ const Book = () => {
                             <div className="divider bordered border-[#ffffff]"></div>
                         </div>) :
                         (
-                            <div className="w-[62%] mt-[30px] mb-[150px]">
-                                <div className="">
-                                    <div className="flex flex-row justify-between mb-[20px]">
-                                        <h4 className="text-[#ffffff] text-[20px] font-bold leading-[24px] tracking-[-0.4px]">Status of all versions</h4>
-                                        <div>
-                                            <HistoryBook />
-                                            {/* <button className="btn "> */}
-                                            <label htmlFor="history_book" className="cursor-pointer btn bg-[#eeeeee] text-[#000000] mr-[10px] hover:text-[#ffffff]">
-                                                Watch History Rent
-                                            </label>
-                                            {/* </button> */}
-                                            <AddVersionBook />
-                                            {/* <button className=""> */}
-                                            <label htmlFor="modal_addVersion" className="cursor-pointer btn w-[140px] bg-gradient-to-r from-indigo-700 to-blue-700 text-[#ffffff] leading-[24px] hover:from-indigo-600 hover:to-blue-600">
-                                                Add version
-                                            </label>
-                                            {/* </button> */}
-                                        </div>
+                            auth?.role == 'admin' && <LocationVersion />
+                            // <div className="w-[62%] mt-[30px] mb-[150px]">
+                            //     <div className="">
+                            //         <div className="flex flex-row justify-between mb-[20px]">
+                            //             <h4 className="text-[#ffffff] text-[20px] font-bold leading-[24px] tracking-[-0.4px]">Status of all versions</h4>
+                            //             <div>
+                            //                 <HistoryBook />
+                            //                 {/* <button className="btn "> */}
+                            //                 <label htmlFor="history_book" className="cursor-pointer btn bg-[#eeeeee] text-[#000000] mr-[10px] hover:text-[#ffffff]">
+                            //                     Watch History Rent
+                            //                 </label>
+                            //                 {/* </button> */}
+                            //                 <AddVersionBook />
+                            //                 {/* <button className=""> */}
+                            //                 <label htmlFor="modal_addVersion" className="cursor-pointer btn w-[140px] bg-gradient-to-r from-indigo-700 to-blue-700 text-[#ffffff] leading-[24px] hover:from-indigo-600 hover:to-blue-600">
+                            //                     Add version
+                            //                 </label>
+                            //                 {/* </button> */}
+                            //             </div>
 
-                                    </div>
-                                    <table className="table w-full border-nones">
-                                        <thead className="text-[#ffffff] font-bold text-[15px]">
-                                            <tr>
-                                                <th>ID Book</th>
-                                                <th>Location</th>
-                                                <th>Status</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody className="font-semibold text-[13px]">
-                                            <tr>
-                                                <td>ABCD6969</td>
-                                                <td>123456</td>
-                                                <td ><div className="badge bg-green-500 gap-2 w-[80px] text-[#ffffff]">Available</div></td>
-                                            </tr>
-                                            <tr>
-                                                <td>ABCD6970</td>
-                                                <td>123457</td>
-                                                <td><div className="badge bg-yellow-500 gap-2 w-[80px] text-[#ffffff]">Booked</div></td>
-                                            </tr>
-                                            <tr>
-                                                <td>ABCD6971</td>
-                                                <td>123458</td>
-                                                <td><div className="badge bg-red-500 gap-2 w-[80px] text-[#ffffff]">Borrowed</div></td>
-                                            </tr>
-                                            <tr>
-                                                <td>ABCD6972</td>
-                                                <td>123459</td>
-                                                <td><div className="badge bg-red-500 gap-2 w-[80px] text-[#ffffff]">Borrowed</div></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                    <div className="divider bordered border-[#ffffff]"></div>
-                                </div>
-                            </div>
+                            //         </div>
+                            //         <table className="table w-full border-nones">
+                            //             <thead className="text-[#ffffff] font-bold text-[15px]">
+                            //                 <tr>
+                            //                     <th>ID Book</th>
+                            //                     <th>Location</th>
+                            //                     <th>Status</th>
+                            //                 </tr>
+                            //             </thead>
+                            //             <tbody className="font-semibold text-[13px]">
+                            //                 <tr>
+                            //                     <td>ABCD6969</td>
+                            //                     <td>123456</td>
+                            //                     <td ><div className="badge bg-green-500 gap-2 w-[80px] text-[#ffffff]">Available</div></td>
+                            //                 </tr>
+                            //                 <tr>
+                            //                     <td>ABCD6970</td>
+                            //                     <td>123457</td>
+                            //                     <td><div className="badge bg-yellow-500 gap-2 w-[80px] text-[#ffffff]">Booked</div></td>
+                            //                 </tr>
+                            //                 <tr>
+                            //                     <td>ABCD6971</td>
+                            //                     <td>123458</td>
+                            //                     <td><div className="badge bg-red-500 gap-2 w-[80px] text-[#ffffff]">Borrowed</div></td>
+                            //                 </tr>
+                            //                 <tr>
+                            //                     <td>ABCD6972</td>
+                            //                     <td>123459</td>
+                            //                     <td><div className="badge bg-red-500 gap-2 w-[80px] text-[#ffffff]">Borrowed</div></td>
+                            //                 </tr>
+                            //             </tbody>
+                            //         </table>
+                            //         <div className="divider bordered border-[#ffffff]"></div>
+                            //     </div>
+                            // </div>
                         )
                 }
                 <div className="w-[62%]">
