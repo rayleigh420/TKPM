279da53e0b07bf01859a92aa4421d5cbdce40581
rayleigh420 <nhatduy0409@gmail.com>
2023-05-11 20:24:39 +0700

    Add delete use

--- Client/src/components/admin/manages/ManageUser.jsx
+++ Client/src/components/admin/manages/ManageUser.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom"
 import { data } from "../../../pages/home"
 import { useContext, useState } from "react"
 import useDebounce from "../../../hooks/useDebounce"
-import { useQuery } from "@tanstack/react-query"
-import { getAllUser } from "../../../api/userApi"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { deleteUser, getAllUser } from "../../../api/userApi"
 import AuthContext from "../../../context/AuthProvider"
+import { toast } from "react-toastify"
 
 const ManageUser = () => {
     const [search, setSearch] = useState('')
@@ -17,6 +18,16 @@ const ManageUser = () => {
         queryFn: () => getAllUser(auth.token)
     })
 
+    const deleteUserMutate = useMutation({
+        mutationFn: (idUser) => deleteUser(idUser),
+        onSuccess: () => {
+            toast.info("Delete User Success!")
+        },
+        onError: () => {
+            toast.error("Delete failed! Please try again")
+        }
+    })
+
     const handleChangeSeach = (e) => {
         setSearch(e.target.value)
     }
@@ -25,8 +36,6 @@ const ManageUser = () => {
         e.preventDefault()
         console.log(search, typeSearch)
     }
-
-
 
     return (
         <>
@@ -79,7 +88,7 @@ const ManageUser = () => {
                                                         </svg>
                                                     </label>
                                                 </span> */}
-                                                <span className="cursor-pointer">
+                                                <span className="cursor-pointer" onClick={() => deleteUserMutate.mutate(item?.user_id)}>
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                     </svg>
